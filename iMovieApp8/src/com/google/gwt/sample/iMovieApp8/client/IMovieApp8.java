cb2d0d23f77833726b895799325336e8252d2311
lucaspelloni <lucas.pelloni@hotmail.com>
2015-10-27 13:33:39 +0100

    initial

---
+++ iMovieApp8/src/com/google/gwt/sample/iMovieApp8/client/IMovieApp8.java
@@ -0,0 +1,13 @@
+package com.google.gwt.sample.iMovieApp8.client;
+
+import com.google.gwt.core.client.EntryPoint;
+
+/**
+ * Entry point classes define <code>onModuleLoad()</code>.
+ */
+public class IMovieApp8 implements EntryPoint {
+	
+	public void onModuleLoad() {
+		
+}
+}
---
+++ iMovieApp8/src/com/google/gwt/sample/iMovieApp8/server/GreetingServiceImpl.java
@@ -0,0 +1,19 @@
+package com.google.gwt.sample.iMovieApp8.server;
+
+import com.google.gwt.sample.iMovieApp8.client.GreetingService;
+import com.google.gwt.sample.iMovieApp8.shared.FieldVerifier;
+import com.google.gwt.user.server.rpc.RemoteServiceServlet;
+
+/**
+ * The server-side implementation of the RPC service.
+ */
+@SuppressWarnings("serial")
+public class GreetingServiceImpl extends RemoteServiceServlet implements GreetingService {
+
+	@Override
+	public String greetServer(String name) throws IllegalArgumentException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	}
+
